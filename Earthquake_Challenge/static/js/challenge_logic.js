941ffe3d857752716768badeebbcd12d2616a4ea
Ruba Dhanaraj <108298416+rubadhanaraj@users.noreply.github.com>
2022-09-22 18:34:28 -0400

    Add files via upload

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -217,7 +217,7 @@ legend.onAdd = function() {
 
 let tectonicData = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 let myStyle = {
-  color: "darkorange",
+  color: "#ea822c",
   weight: 2.5
 }
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
